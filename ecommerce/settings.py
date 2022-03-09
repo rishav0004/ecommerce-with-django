ab5452092f96c65e272264da350e269fd8370bd1
Rishav <rishabh5.mahajan@gmail.com>
2022-03-09 23:02:53 +0530

    master

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from re import template
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shopping'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
--- shopping/views.py
+++ shopping/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def store(request):
+    context = {}
+    return render(request,'shopping/store.html',context)
+
+
+def cart(request):
+    context = {}
+    return render(request,'shopping/cart.html',context)
+
+
+def checkout(request):
+    context = {}
+    return render(request,'shopping/checkout.html',context)
+
+    
